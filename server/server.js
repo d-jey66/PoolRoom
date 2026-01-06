d6802b10fed96a8d5fbb62ab8eceb832ad9c21c2
dachi <jananashvilidachi@gmail.com>
2026-01-06 21:13:07 +0400

    version 1

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://pool-room.vercel.app/',  
+    origin: 'https://pool-room.vercel.app',  
     credentials: true
 }));
 
