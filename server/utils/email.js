23134a9257ba44724246deb8a7a8e16d3e6eb8f9
dachi <jananashvilidachi@gmail.com>
2026-01-08 22:52:41 +0400

    version 1

--- server/utils/email.js
+++ server/utils/email.js
@@ -9,7 +9,12 @@ const sendEmail = async ({ to, subject, html }) => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10_000,
+    greetingTimeout: 10_000,
+    socketTimeout: 10_000,
   });
+
+  await transporter.verify(); // this will expose the real issue
 
   await transporter.sendMail({
     from: `"Pool Room" <${process.env.EMAIL_USER}>`,
