1eec4c41dca309475372e9b724b7613b567092bd
dachi <jananashvilidachi@gmail.com>
2026-01-08 19:11:11 +0400

    version 1

--- server/controllers/reservation.controller.js
+++ server/controllers/reservation.controller.js
@@ -117,7 +117,7 @@ export const createReservation = async (req, res) => {
 
       console.log('Attempting to send email to:', userDoc.email);
       
-      sendEmail({
+      await sendEmail({
         to: userDoc.email,
         subject: `🎱 Reservation Confirmed - ${user} at Pool Room`,
         html: emailHtml
