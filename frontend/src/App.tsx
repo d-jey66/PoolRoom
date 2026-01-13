b4272c596484c4f255c8f4fbf6b58c10ec9f3d88
dachi <jananashvilidachi@gmail.com>
2026-01-14 03:50:37 +0400

    version 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -42,7 +42,7 @@ function App() {
         />
         
         <Route
-        path="/payment"
+        path="/payment/:reservationId"
         element={
           <ProtectedRoute>
             <Payment />
