54db3126deceea93155046d454a38f9fdfb90227
dachi <jananashvilidachi@gmail.com>
2026-01-16 22:00:34 +0400

    version 3

--- server/routers/rating.router.js
+++ server/routers/rating.router.js
@@ -4,13 +4,13 @@ import {
   getAverageRating, 
   getUserRating 
 } from "../controllers/rating.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const RatingRouter = express.Router();
 
 RatingRouter.get("/average", getAverageRating);
 
-RatingRouter.post("/", protectRoute, createOrUpdateRating);
-RatingRouter.get("/user", protectRoute, getUserRating);
+RatingRouter.post("/", protect, createOrUpdateRating);
+RatingRouter.get("/user", protect, getUserRating);
 
 export default RatingRouter;
