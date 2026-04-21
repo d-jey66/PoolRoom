3c8c89fcc09046eec1429c894fe17d658962b365
d-jey66 <jananashvilidachi@gmail.com>
2026-04-21 22:13:30 +0400

    version 2.0 application added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,16 +4,13 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
-import { ThemeProvider } from "./context/ThemeProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
       <BrowserRouter>
         <AuthProvider>
           <App />
         </AuthProvider>
       </BrowserRouter>
-    </ThemeProvider>
   </StrictMode>,
 );
