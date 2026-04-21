d15da17fcd5a2da5c2a115333e8dd964cab62bc9
d-jey66 <jananashvilidachi@gmail.com>
2026-04-21 22:15:38 +0400

    version 2.0 application added

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -144,7 +144,6 @@ export default function Navbar() {
                     Sign Up
                   </Button>
                   </Link>
-                  <ThemeToggle />
               </>
             )}
           </div>
